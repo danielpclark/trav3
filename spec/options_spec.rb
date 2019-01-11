1945d69739dcc2c35d68a295a5b5103ab068e532
Daniel P. Clark <6ftdan@gmail.com>
2019-01-11 13:45:32 -0500

    Trav3::Options spec coverage & fixes - #1

--- spec/options_spec.rb
+++ spec/options_spec.rb
@@ -5,25 +5,38 @@ RSpec.describe Trav3::Options do
 
   describe '#build' do
     it 'creates a url friendly parameter list' do
-      expect('?a=b&c=d').to eq opts.to_s
+      expect(opts.to_s).to eq '?a=b&c=d'
     end
 
     it 'appends to current options' do
       opts.build(e: :f)
-      expect('?a=b&c=d&e=f').to eq opts.to_s
+      expect(opts.to_s).to eq '?a=b&c=d&e=f'
     end
   end
 
   describe '#remove' do
     it 'removes a parameter' do
       opts.remove(:a)
-      expect('?c=d').to eq opts.to_s
+      expect(opts.to_s).to eq '?c=d'
+    end
+
+    it 'can return an empty string when empty' do
+      opts.remove(:a)
+      opts.remove(:c)
+      expect(opts.to_s).to be_empty
     end
   end
 
   describe '#to_h' do
     it 'creates a valid hash from the options instance' do
       expect(opts.to_h).to eq({'c' => 'd', 'a' => 'b'})
+    end
+  end
+
+  describe '#+' do
+    it 'merges two options together' do
+      result = opts.build(w: :x) + opts.build(y: :z)
+      expect(result.to_s).to include("w=x&y=z")
     end
   end
 end
