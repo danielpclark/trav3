e4811ef3023c3b5007f7f93368db64cdd41067f1
Daniel P. Clark <6ftdan@gmail.com>
2019-02-01 13:14:27 -0500

    Typo fix as per @varyonic - #62

--- lib/trav3/error/invalid_repository.rb
+++ lib/trav3/error/invalid_repository.rb
@@ -3,7 +3,7 @@
 module Trav3
   class InvalidRepository < StandardError
     def message
-      "The repository format was invlaid.
+      "The repository format was invalid.
   You must either provide the digit name for the repository,
   or `user/repo` or `user%2Frepo` as the name."
     end
